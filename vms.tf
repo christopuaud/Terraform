324851a95dec8f9fa030acda0191d624c97de41f
christopuaud <puaud.christopher@outlook.fr>
2024-06-19 14:46:50 +0200

    vm.tf

--- vms.tf
+++ vms.tf
@@ -1,4 +1,30 @@
 ressource "proxmox_vm_qmu" "vm-tdupre" {
   count    = 2
   vmid     = var.vmid[count.index]
+  name     = "i2-tdupre-0${count.index + 1}"
+  desc     = "vm tdupre test terraform"
+  target_node = "hv_epyc-01"
+  scsihw   = "virtio-scsi-pci"
+
+  automatic_reboot = true
+  clone = "debian-bookworw-i2"
+  full_clon = false
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage ='i2'
+          size    = 10
+          format  = "qcow2"
+        }
+      }
+    }
+  }
+  cloudinit_cdrom_storage ='I2"
+  
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
 }
